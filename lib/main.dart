e01a470454f66aed2f9d04530c55153392fdc838
DHANESWAR BHAKAT <dhaneswarbhakat2000@gmail.com>
2024-03-26 17:31:33 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:mausam_app/Activity/home.dart';
+import 'package:mausam_app/Activity/loading.dart';
+import 'package:mausam_app/Activity/location.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
